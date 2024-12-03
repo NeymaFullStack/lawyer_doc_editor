8b2f008820f069fc5a7ffb661316529a4945e693
NeymaFullStack <cow.dev423@gmail.com>
2024-12-04 01:30:08 +0530

    add workspace and auth token in header 1

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 import { HOST_API } from "@/config-global";
 
-// const axiosInstance = axios.create({ baseURL: HOST_API });
-const axiosInstance = axios.create({ baseURL: "http://localhost:7003" });
+const axiosInstance = axios.create({ baseURL: HOST_API });
 
 axiosInstance.defaults.headers.common["Content-Type"] =
   "application/x-www-form-urlencoded";
@@ -12,8 +11,8 @@ axiosInstance.interceptors.response.use(
   (res) => res,
   (error) =>
     Promise.reject(
-      (error.response && error.response.data) || "Something went wrong",
-    ),
+      (error.response && error.response.data) || "Something went wrong"
+    )
 );
 
 export default axiosInstance;
