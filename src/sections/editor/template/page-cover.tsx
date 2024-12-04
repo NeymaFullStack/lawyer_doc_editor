f22ac6fc23ea0a78efc5b143d789226f966ed178
NeymaFullStack <cow.dev423@gmail.com>
2024-12-04 16:28:37 -0500

    build skeleton pageCover and pageTableofContents in preview

---
+++ src/sections/editor/template/page-cover.tsx
@@ -0,0 +1,12 @@
+import { PageLayouts } from "./page-layouts";
+
+export const PageCover = () => {
+  return (
+    <PageLayouts>
+      <p className="logo text-2xl uppercase">Lexington </p>
+      <p className="documentTitle text-7xl text-center font-medium h-full flex items-center">
+        Franchise Agreement
+      </p>
+    </PageLayouts>
+  );
+};
